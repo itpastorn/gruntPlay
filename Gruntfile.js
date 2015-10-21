b659dd43843406b9e757c5cd561ead3114284884
Lars Gunther <gunther@keryx.se>
2015-10-21 17:38:21 +0200

    Testat console.log och grunt.log.writeln

---
+++ Gruntfile.js
@@ -0,0 +1,6 @@
+module.exports = function(grunt) {
+    grunt.registerTask('default', function() {
+        // console.log("Detta är Grunt som talar");
+        grunt.log.writeln("Detta är Grunt som talar");
+    });
+}
